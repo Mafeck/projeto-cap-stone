381ed0b35748836625edd69d89281bff55997b70
FelipeMatheus1999 <felipe_cavalcante7939@hotmail.com>
2021-11-11 20:54:49 -0300

    feat: Added providers user and people

---
+++ src/providers/People/index.tsx
@@ -0,0 +1,54 @@
+import { ReactNode, useState, useContext, createContext } from "react";
+
+interface Address {
+  road: string;
+  zipCode: string;
+  district: string;
+  houseNumber: string;
+}
+
+interface Comments {
+  title: string;
+  comment: string;
+  id: number;
+}
+
+interface People {
+  name: string;
+  cpf: string;
+  genre: string;
+  naturalness: string;
+  nationality: string;
+  fatherName: string;
+  motherName: string;
+  qualification: string;
+  company: string;
+  phone: string;
+  type: string;
+  maritalStatus: string;
+  address: Address[];
+  comments: Comments[];
+}
+
+interface PeopleContextData {
+  people: People[];
+  setPeople: (props: People[]) => void;
+}
+
+interface PeopleProviderProps {
+  children: ReactNode;
+}
+
+const PeopleContext = createContext<PeopleContextData>({} as PeopleContextData);
+
+export const PeopleProvider = ({ children }: PeopleProviderProps) => {
+  const [people, setPeople] = useState<People[]>([]);
+
+  return (
+    <PeopleContext.Provider value={{ people, setPeople }}>
+      {children}
+    </PeopleContext.Provider>
+  );
+};
+
+export const usePeople = () => useContext(PeopleContext);
---
+++ src/providers/User/index.tsx
@@ -0,0 +1,38 @@
+import { useState, useContext, ReactNode, createContext } from "react";
+
+interface User {
+  name: string;
+  email: string;
+  password: string;
+  id: number;
+}
+
+interface UserContextData {
+  user: User[];
+  setUser: (props: User[]) => void;
+  token: string;
+  setToken: (props: string) => void;
+}
+
+interface UserProviderProps {
+  children: ReactNode;
+}
+
+const UserContext = createContext<UserContextData>({} as UserContextData);
+
+export const UserProvider = ({ children }: UserProviderProps) => {
+  const [user, setUser] = useState(
+    JSON.parse(localStorage.getItem("@user:haki")!) || []
+  );
+  const [token, setToken] = useState(
+    JSON.parse(localStorage.getItem("@token:haki")!) || ""
+  );
+
+  return (
+    <UserContext.Provider value={{ user, setUser, token, setToken }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export const useUser = () => useContext(UserContext);
