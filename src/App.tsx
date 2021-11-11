b3ed98e3c89a1cedfa27b6b6879862f5315e35f6
Mia Ferrari <miaa_ferrari@hotmail.com>
2021-11-10 21:40:22 -0300

    feat: remove Input component from app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import Input from "./components/Input";
 import { Global } from "./style/global";
 
 function App() {
   return (
     <div className="App">
       <Global />
-      <Input placeholder={'Name'} />
     </div>
   );
 }
