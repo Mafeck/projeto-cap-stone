27fd9d477471401f73a4542c15e4a79a53322c3b
FelipeMatheus1999 <felipe_cavalcante7939@hotmail.com>
2021-11-11 16:04:25 -0300

    feat: Added component card home

---
+++ src/components/CardHome/styles.ts
@@ -0,0 +1,38 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 330px;
+  height: 75px;
+  display: flex;
+  border-radius: 5px;
+  margin: 10px auto;
+  cursor: pointer;
+
+  .divImage {
+    background: var(--color-primary);
+    width: 78px;
+    height: 100%;
+    border-radius: 5px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  main {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    padding-left: 10px;
+
+    h3 {
+      font-weight: 600;
+    }
+
+    p {
+      color: var(--gray-50);
+      width: 240px;
+      font-size: 12px;
+      font-weight: 500;
+    }
+  }
+`;
