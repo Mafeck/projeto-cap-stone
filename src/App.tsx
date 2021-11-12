d34dc8b09ce8e060e24001fe07ddcb9d1afc299c
Maria <mfb171201@gmail.com>
2021-11-12 08:39:36 -0400

    feat: added Header Homepage mobile/desktop; added color black and white.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
 import { Global } from "./style/global";
-import Header from "./components/Header";
-
 function App() {
   return (
     <div className="App">
       <Global />
-      <Header/>
     </div>
   );
 }
