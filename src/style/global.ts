986f8d0445615538bb9b30e57d70750632ef075d
Felipe Matheus Rabelo Cavalcante <85685562+FelipeMatheus1999@users.noreply.github.com>
2021-11-13 14:06:40 -0300

    Update global.ts

--- src/style/global.ts
+++ src/style/global.ts
@@ -15,11 +15,12 @@ export const Global = createGlobalStyle`
         --gray-50: #878787;
         --gray-20: #E0E0E0;
         --gray-0: #F5F5F5;
+        --white: #FFFFFF;
+        --black: #000000;
         --negative: #E60000;
         --warning: #FFCD07;
         --success: #168821;
         --information: #155BCB;
-        --black: #000000;
     }
 
     body {
