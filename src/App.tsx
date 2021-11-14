4a41e735a7cee55fd1856042968fd23803eb6a7f
Maria <mfb171201@gmail.com>
2021-11-14 18:26:03 -0400

    fix: removed from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,8 @@
 import { Global } from "./style/global";
-import CardDashboard from "./components/CardDashboard";
 
 function App() {
   return (
     <div className="App">
-      <CardDashboard
-        title="rfewrewrwe"
-        description="gfwewf"
-        image="fefsdf"
-        background="red"
-        backgroundBall="blue"
-      />
       <Global />
     </div>
   );
