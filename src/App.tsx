44770df4364c1633242d616789d2948cd06307d2
Mia Ferrari <miaa_ferrari@hotmail.com>
2021-11-15 10:48:03 -0300

    feat: handleDelete function initiated

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,12 @@
-import Comments from "./components/CardComments";
 import Router from "./routes";
 import { Global } from "./style/global";
 
 function App() {
 
-  const handleClick = () => {
-    localStorage.setItem("@token:haki", 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im1pYUBnbWFpbC5jb20iLCJpYXQiOjE2MzY5MTE1MTgsImV4cCI6MTYzNjkxNTExOCwic3ViIjoiMyJ9.p1W00wTlXgPtoh5wc_OVN4FFt47jrqGXvJ6v83w90Gc')
-  }
   return (
     <div className="App">
       <Global />
       <Router />
-      <Comments />
-      <button onClick={handleClick}>Clique</button>
     </div>
   );
 }
--- src/components/CardComments/index.jsx
+++ src/components/CardComments/index.jsx
@@ -1,9 +1,29 @@
 import { ReactComponent as Trash } from '../../assets/trash.svg'
 import { Container } from './style';
+import api from '../../services';
+import { useUser } from '../../providers/User';
+import { useState } from 'react';
 
 const Comments = () => {
+    const [update, setUpdate] = useState("");
+    const [createComment, setCreateComment] = useState(false); // p/ o modal
 
-    
+    const {token} = useUser();
+
+    const handleUpdate = (data) => {
+
+        api
+            .patch('', data, {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+            })
+            .then((response) => {
+                setUpdate(response)
+                setCreateComment(false);
+            })
+            .catch((error) => console.log(error))
+    }
 
     return(
         <Container>
