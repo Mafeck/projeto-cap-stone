8b236384ca659fb394637883d6d8659d3fd6d470
Maria <mfb171201@gmail.com>
2021-11-15 13:36:17 -0400

    fix: removed from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import { Global } from "./style/global";
-import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
     <div className="App">
       <Global />
-      <Dashboard/>
     </div>
   );
 }
